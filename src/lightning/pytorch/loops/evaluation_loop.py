cfb98bca323d660ea79c3f6dd5226ae93549adcd
Henning Buhl <henning.buhl@yahoo.de>
2023-08-20 12:06:47 +0200

    Updated checks for dataloader_idx and max_batches
    The desired behavior works, but we have to add dataloader_idx=0 to the on_test and on_validation methods. This is a small hack and the next commit should amend the check to make the methods work without dataloader_idx=0

--- src/lightning/pytorch/loops/evaluation_loop.py
+++ src/lightning/pytorch/loops/evaluation_loop.py
@@ -207,7 +207,7 @@ class _EvaluationLoop(_Loop):
         if trainer.datamodule is not None:
             allow_zero_length |= trainer.datamodule.allow_zero_length_dataloader_with_multiple_devices
 
-        if self._is_sequential:
+        if self._is_sequential or self._is_sequence:
             self._max_batches = []
             for dl in combined_loader.flattened:
                 # determine number of batches
@@ -457,7 +457,7 @@ class _EvaluationLoop(_Loop):
         assert self._combined_loader is not None
         _verify_dataloader_idx_requirement(
             (step_hook, batch_start_hook, batch_end_hook),
-            self._is_sequential and self.num_dataloaders > 1,
+            (self._is_sequential or self._is_sequence) and self.num_dataloaders > 1,
             self._stage,
             trainer.lightning_module,
         )
